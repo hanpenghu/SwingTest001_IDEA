2a9dbcb77d319303fad6c9822e72e701068afccf
hanpenghu <563909408@qq.com>
2017-07-01 21:54:36 +0800

    Initial commit

---
+++ src/com/hanhan/test/SwingTest001.java
@@ -0,0 +1,20 @@
+package com.hanhan.test;
+
+import javax.swing.*;
+
+/**
+ * Created by hanhan on 2017-07-01.
+ */
+public class SwingTest001 {
+    public  void f(){
+        int width=300;
+         int heigth=200;
+        JFrame jf=new JFrame("Hello Swing !!!");
+        jf.setSize(width,heigth);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setVisible(true);
+    }
+    public static void main(String[]args){
+       new SwingTest001().f();
+    }
+}
